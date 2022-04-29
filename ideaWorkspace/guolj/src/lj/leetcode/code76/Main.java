4a9b1e2bc2f3849343f6ac80724ecd19f211fb9b
ghj <ghjghjghjghj>
2022-04-29 19:40:32 +0800

    76

---
+++ ideaWorkspace/guolj/src/lj/leetcode/code76/Main.java
@@ -0,0 +1,12 @@
+package lj.leetcode.code76;
+
+/**
+ * @author 86187
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s = "aaaaaaaaaaaabbbbbcdd", t = "abcdd";
+        Solution a = new Solution();
+        System.out.println(a.minWindow(s,t));
+    }
+}
