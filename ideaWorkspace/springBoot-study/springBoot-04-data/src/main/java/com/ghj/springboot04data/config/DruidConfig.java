62fe65c29dd2098793f0e00db525aef79f0552bb
ghj <ghjghjghjghj>
2022-05-04 10:00:47 +0800

    Druid监视器与过滤器

--- ideaWorkspace/springBoot-study/springBoot-04-data/src/main/java/com/ghj/springboot04data/config/DruidConfig.java
+++ ideaWorkspace/springBoot-study/springBoot-04-data/src/main/java/com/ghj/springboot04data/config/DruidConfig.java
@@ -2,7 +2,9 @@ package com.ghj.springboot04data.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,13 +24,29 @@ public class DruidConfig {
     public DruidDataSource druidDataSource(){
         return new DruidDataSource();
     }
+
+    /**
+     * 因为SpringBoot包含了servlet， 没有对应的web.xml文件， 所以我们不能设置到web.xml
+     * 但是因为他在SpringBoot里面， 我们可以通过@Bean设置到里面， 自己加一些配置。
+     * @return
+     */
     @Bean
-    public ServletRegistrationBean statViewServlet(){
+    public ServletRegistrationBean<StatViewServlet> statViewServlet(){
         ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
         Map<String, String> map = new HashMap<>();
         map.put("loginUsername","ghj");
         map.put("loginPassword","123456");
         map.put("allow","");
+        bean.setInitParameters(map);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> map = new HashMap<>();
+        map.put("exclusions","*.jsp, *.css, /druid/*");
         bean.setInitParameters(map);
         return bean;
     }
