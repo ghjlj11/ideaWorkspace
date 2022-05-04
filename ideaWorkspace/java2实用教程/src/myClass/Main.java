2d05e29fcc4f704fba7c042596664035efbc9d8c
ghj <ghjghjghjghj>
2022-05-04 10:02:17 +0800

    反射

--- ideaWorkspace/java2实用教程/src/myClass/Main.java
+++ ideaWorkspace/java2实用教程/src/myClass/Main.java
@@ -6,5 +6,9 @@ package myClass;
 public class Main {
     public static void main(String[] args) {
         System.out.println(s1.P);
+        Class<s1> s1Class = s1.class;
+        System.out.println(s1Class.getClassLoader());
+        Class<? extends Class> aClass = s1.class.getClass();
+        System.out.println(aClass);
     }
 }
