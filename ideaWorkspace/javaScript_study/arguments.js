7659b08aa925ef09b126df1378dd35f3e4646c1b
ghj <2367792309@qq.com>
2022-08-01 00:34:22 +0800

    js

---
+++ ideaWorkspace/javaScript_study/arguments.js
@@ -0,0 +1,13 @@
+//形参可以任意，为了后续方便使用.
+function fun(a, b) {
+    console.log(a);
+    console.log(b);
+    console.log(arguments.length);
+    //当前函数
+    console.log(arguments.callee);
+    //第几个实参
+    console.log(arguments[3]);
+    alert(new Date);
+}
+//可以传多个参数，都会接收
+fun(1, 2, 3, 45, 5);
