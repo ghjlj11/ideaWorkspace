4c846cb17abdd0a9fcc649b68b4810a8ab07dd6b
ghj <2367792309@qq.com>
2022-11-03 22:36:52 +0800

    测试 Stream

--- ideaWorkspace/java2实用教程/afterjava8/src/main/java/com/ghj/stream/StreamTest4.java
+++ ideaWorkspace/java2实用教程/afterjava8/src/main/java/com/ghj/stream/StreamTest4.java
@@ -2,8 +2,7 @@ package com.ghj.stream;
 
 import com.ghj.functioninterface.Student;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -12,7 +11,24 @@ import java.util.stream.Collectors;
 public class StreamTest4 {
     public static void main(String[] args) {
         List<Student> list = StudentsUtil.getList();
+        // 使用Collector转化为别的任意集合
         HashSet<String> collect = list.stream().map(Student::getName).collect(Collectors.toCollection(HashSet::new));
         System.out.println(collect);
+        // 使用Collector 计算
+        Double collect1 = list.stream().collect(Collectors.averagingInt(Student::getAge));
+        System.out.println(collect1);
+        DoubleSummaryStatistics collect3 = list.stream().collect(Collectors.summarizingDouble(Student::getAge));
+        System.out.println(collect3.getMax());
+        System.out.println(collect3.getMin());
+        System.out.println(collect3.getAverage());
+
+        // 分组  无线分组
+        Map<String, Map<Integer, List<Student>>> collect2 = list.stream().collect(Collectors.groupingBy(Student::getName, Collectors.groupingBy(Student::getAge)));
+        System.out.println(collect2);
+        // join 连接字符串
+        String collect4 = list.stream().map(Student::getName).collect(Collectors.joining(",", "----", "----"));
+        String[] split = collect4.split(",");
+        System.out.println(collect4);
+        System.out.println(Arrays.toString(split));
     }
 }
