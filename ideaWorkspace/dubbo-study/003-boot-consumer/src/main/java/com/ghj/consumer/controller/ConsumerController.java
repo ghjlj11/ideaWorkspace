aadf6c3941c39da90c47c0c45aecb447cb1c8178
ghj <2367792309@qq.com>
2023-06-12 22:06:47 +0800

    springboot整合dubbo直连方式

--- ideaWorkspace/dubbo-study/003-boot-consumer/src/main/java/com/ghj/consumer/controller/ConsumerController.java
+++ ideaWorkspace/dubbo-study/003-boot-consumer/src/main/java/com/ghj/consumer/controller/ConsumerController.java
@@ -18,6 +18,7 @@ public class ConsumerController {
 
     /**
      * 该注解配置与xml基本一样， 同样包含接口全限定类名， 版本号， 重新请求次数， 超时时间等配置
+     * 不通过注册中心，用直连的方式则需要加上 url = "dubbo://127.0.0.1:20880" 属性， 就是对应dubbo协议的ip和端口
      */
     @DubboReference(interfaceClass = UserProviderService.class, check = false)
     UserProviderService userProviderService;
