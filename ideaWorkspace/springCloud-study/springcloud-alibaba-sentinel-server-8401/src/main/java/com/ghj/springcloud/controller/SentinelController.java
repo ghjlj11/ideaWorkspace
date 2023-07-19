ddd545009ce33475082769e070b10d5b6cf26118
ghj <2367792309@qq.com>
2023-07-20 00:15:01 +0800

    sentinel 熔断

--- ideaWorkspace/springCloud-study/springcloud-alibaba-sentinel-server-8401/src/main/java/com/ghj/springcloud/controller/SentinelController.java
+++ ideaWorkspace/springCloud-study/springcloud-alibaba-sentinel-server-8401/src/main/java/com/ghj/springcloud/controller/SentinelController.java
@@ -1,15 +1,21 @@
 package com.ghj.springcloud.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author guohuanjun1
- * @description: 测试接口
- * @date 2023/7/18 16:18
+ * @Description:
+ * @author guohuanjun
+ * @date 2023/7/19  11:50
  */
 @RestController
 @RequestMapping("/test")
@@ -24,5 +30,21 @@ public class SentinelController {
     public String testB(){
         System.out.println("当前线程:" + Thread.currentThread().getName() + "当前时间:" + LocalTime.now().getSecond() + "-----testB");
         return "=====test B";
+    }
+
+    @RequestMapping("/d")
+    public String testD(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "=====test D";
+    }
+
+    @RequestMapping("/e/{i}")
+    public String testE( @PathVariable Integer i){
+        int h = 10 / i;
+        return "=====test E..." + h;
     }
 }
